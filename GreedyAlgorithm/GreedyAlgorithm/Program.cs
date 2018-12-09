cc005f7b486902c4c19a92ca7a05b803129830cf
Joy Trinh <mongtuyen149@gmail.com>
2018-12-09 20:05:39 +0700

    Give some initial ideas about the greedy algorithm

--- GreedyAlgorithm/GreedyAlgorithm/Program.cs
+++ GreedyAlgorithm/GreedyAlgorithm/Program.cs
@@ -10,7 +10,25 @@ namespace GreedyAlgorithm
     {
         static void Main(string[] args)
         {
-
+            /*
+            1$ = 100cents
+            1 quarter = 25cents
+            1 dime = 10cents
+            1 nickel = 5cents
+            1 penny = 1cent
+            the heart of the greedy algorithm, which is always using the largest coin possible
+            prompt the user for an amount of change
+            - The amount must make sense
+            - What values are accepted
+            - get_float
+            always use the largest coin possible
+            - How many coins to be returned
+            - amount to be returned
+            keep track of coins used
+            print the final number of coins
+            */
+            
+            
         }
     }
 }
