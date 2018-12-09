17f9bd1bad9dca255b760029c3ba0ae47d01aeb8
Joy Trinh <mongtuyen149@gmail.com>
2018-12-09 22:02:28 +0700

    Use modulo math

--- GreedyAlgorithm/GreedyAlgorithm/Program.cs
+++ GreedyAlgorithm/GreedyAlgorithm/Program.cs
@@ -14,13 +14,18 @@ namespace GreedyAlgorithm
             decimal number;
             Console.WriteLine("Please give the amount in dollars to change: ");
             value = Console.ReadLine();
+            //Check the validity of data input
             if (decimal.TryParse(value, out number))
                 Console.WriteLine(number);
             else
                 Console.WriteLine("Unable to parse '{0}'",value);
+            
             //Convert $ to cents
             decimal cents = number * 100;
             int count = 0;
+
+            //Using while loop
+            /*                        
             while (cents >= 25)
             {
                 count++;
@@ -41,7 +46,35 @@ namespace GreedyAlgorithm
                 count++;
                 cents = cents - 1;
             }
+            */
+
+            //Using modulo math
+            while (cents >= 25)
+            {
+                int i = (int)cents / 25;
+                count += i;
+                cents = cents - 25 * i;                 
+            }
+            while (cents >= 10)
+            {
+                int i = (int)cents / 10;
+                count += i;
+                cents = cents - 10 * i;
+            }
+            while (cents >= 5)
+            {
+                int i = (int)cents / 5;
+                count += i;
+                cents = cents - 5 * i;
+            }
+            while (cents >= 1)
+            {
+                int i = (int)cents;
+                count += i;
+                cents = cents - i;
+            }
             Console.WriteLine("The amount of coins to be returned is: " + count);
-        }
+            Console.ReadLine();
+        }        
     }
 }
