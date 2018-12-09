e78480e411e9fc2dd004468685ea19aa72fdeb70
Joy Trinh <mongtuyen149@gmail.com>
2018-12-09 20:36:51 +0700

    Finish the project with while loop

--- GreedyAlgorithm/GreedyAlgorithm/Program.cs
+++ GreedyAlgorithm/GreedyAlgorithm/Program.cs
@@ -35,7 +35,30 @@ namespace GreedyAlgorithm
                 Console.WriteLine(number);
             else
                 Console.WriteLine("Unable to parse '{0}'",value);
-
+            //Convert $ to cents
+            decimal cents = number * 100;
+            int count = 0;
+            while (cents >= 25)
+            {
+                count++;
+                cents = cents - 25;
+            }
+            while (cents >= 10)
+            {
+                count++;
+                cents = cents - 10;
+            }
+            while (cents >= 5)
+            {
+                count++;
+                cents = cents - 5;
+            }
+            while (cents >= 1)
+            {
+                count++;
+                cents = cents - 1;
+            }
+            Console.WriteLine("The amount of coins to be returned is: " + count);
         }
     }
 }
